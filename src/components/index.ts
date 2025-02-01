1d0724d2ca8896ca85547a5a3e23787f4a893cbf
mycahfrn <mikaelfernandezj@gmail.com>
2025-02-01 15:58:54 -0400

    Initial Commit

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Footer from "./Footer";
-import Toast from "./Toast";
-import Heart from "./Heart";
-import Button from "./Button";
-import { Loader } from "./Icons/Loader";
-
-export { Footer, Toast, Heart, Button, Loader };
--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import AuthorizePage from "./auth";
-import GeneratePlaylistPage from "./generate-playlist";
-import HelpPage from "./help";
-import ViewPlalistPage from "./view-playlist";
-
-export { AuthorizePage, GeneratePlaylistPage, HelpPage, ViewPlalistPage };
